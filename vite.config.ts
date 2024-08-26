1253846343c712954d0127cb325f774c7fd7d100
Wildiney Di Masi <wildiney@gmail.com>
2024-08-26 14:45:12 -0300

    config vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa';
+import { configDefaults } from 'vitest/config';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -29,4 +31,26 @@ export default defineConfig({
       }
     })
   ],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setupTests.ts',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        ...configDefaults.exclude,
+        '**/e2e/**',
+        '**/node_modules/**',
+        '**/postcss.config.js',
+        '**/tailwind.config.js',
+        '**/vite.config.ts',
+        '**/tsconfig.json',
+        '**/*.d.ts',
+        '**/*.test.ts',
+        '**/*.test.tsx',
+        '**/main.tsx',
+      ]
+    },
+  }
 })
